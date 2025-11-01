159513be8a2db93d14f8bb4a68f0202c1eb3e2c1
Phan Van Tan <tana2k53nvt@gmail.com>
2025-11-02 02:10:48 +0700

    edit

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders'
+    'corsheaders',
     'rest_framework',  
     'cloudinary',
     'channels',
