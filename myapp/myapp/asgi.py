31f931e28ab9854447b8e6e8a4646afe313b7a94
Phan Van Tan <tana2k53nvt@gmail.com>
2025-11-02 15:32:46 +0700

    shshs

--- myapp/myapp/asgi.py
+++ myapp/myapp/asgi.py
@@ -1,14 +1,35 @@
+# import os
+# from django.core.asgi import get_asgi_application
+# import django
+# from channels.routing import ProtocolTypeRouter, URLRouter
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myapp.settings')
+# django.setup()
+# from Middleware.wsMiddleware import JWTAuthMiddleware
+# import chat.routing
+# import notifications.routing
+
+# application = ProtocolTypeRouter({
+#     "http": get_asgi_application(),
+#     "websocket": JWTAuthMiddleware(
+#         URLRouter(
+#             chat.routing.websocket_urlpatterns +
+#             notifications.routing.websocket_urlpatterns
+#         )
+#     ),
+# })
 import os
 import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myapp.settings')
+django.setup()
 from channels.security.websocket import OriginValidator
 from Middleware.wsMiddleware import JWTAuthMiddleware
 import chat.routing
 import notifications.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myapp.settings')
-django.setup()
+
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
