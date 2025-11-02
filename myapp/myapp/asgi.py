0db1cd9d05dc7dd0d30333138fe383273945cdcf
Phan Van Tan <tana2k53nvt@gmail.com>
2025-11-02 15:22:36 +0700

    shsh

--- myapp/myapp/asgi.py
+++ myapp/myapp/asgi.py
@@ -1,20 +1,27 @@
 import os
-from django.core.asgi import get_asgi_application
 import django
+from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myapp.settings')
-django.setup()
+from channels.security.websocket import OriginValidator
 from Middleware.wsMiddleware import JWTAuthMiddleware
 import chat.routing
 import notifications.routing
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myapp.settings')
+django.setup()
+
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": JWTAuthMiddleware(
-        URLRouter(
-            chat.routing.websocket_urlpatterns +
-            notifications.routing.websocket_urlpatterns
-        )
+    "websocket": OriginValidator(
+        JWTAuthMiddleware(
+            URLRouter(
+                chat.routing.websocket_urlpatterns +
+                notifications.routing.websocket_urlpatterns
+            )
+        ),
+        allowed_origins=[
+            "https://socialnetwork-su0z.onrender.com",
+            "http://localhost:3000",
+        ]
     ),
 })
