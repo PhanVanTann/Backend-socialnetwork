776aafa84eecbf2cc4b23909f2dd5984902679b5
Phan Van Tan <tana2k53nvt@gmail.com>
2026-04-17 22:52:04 +0700

    hh

--- myapp/user_auth/urls.py
+++ myapp/user_auth/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
     path('googleLogin/', GoogleLoginView.as_view(), name='google_login'),
-    path('refresh-token/',AuthView.as_view(),name="refresh-token" )
+    path('refresh-token/',AuthView.as_view(),name="refresh-token" ),
+    path('health/', lambda request: JsonResponse({"status": "ok"}), name='health_check'),
 
 ] 
