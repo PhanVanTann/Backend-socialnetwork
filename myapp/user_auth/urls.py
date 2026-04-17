fb1b2161cd5a9ba83a617537225092ab2223e52d
Phan Van Tan <tana2k53nvt@gmail.com>
2026-04-18 00:47:19 +0700

    kk

--- myapp/user_auth/urls.py
+++ myapp/user_auth/urls.py
@@ -1,6 +1,8 @@
 
 from django.urls import path,include
 from .views import *
+from django.http import JsonResponse
+
 urlpatterns = [
     path('verifyEmail/', EmailView.as_view(), name='verify_email'),
     path('login/', LoginView.as_view(), name='login'),
